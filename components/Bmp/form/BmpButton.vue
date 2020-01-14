bfaf867d3452881235de970d4c276842dcb2c576
Blue <azhanging@qq.com>
2020-01-14 22:44:15 +0800

    update

---
+++ components/Bmp/form/BmpButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="bmp-button" :type="type">
+    <slot/>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: "bmp-button",
+    props: ['type']
+  }
+</script>
+
+<style scoped lang="scss">
+  @import '../config';
+
+  .bmp-button {
+    padding: $form-elm-pd;
+  }
+</style>
